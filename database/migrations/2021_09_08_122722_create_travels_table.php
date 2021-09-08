95c6107738e91b2fcb48d92a8a9449927a1f4fb0
Marco Tessitore <marcotexcode@gmail.com>
2021-09-08 14:54:54 +0200

    creazione tabella e migrazione nel database

---
+++ database/migrations/2021_09_08_122722_create_travels_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 80);
+            $table->string('people', 10);
+            $table->string('validity', 50);
+            $table->text('locality');
+            $table->text('description');
+            $table->float('price', 4, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
